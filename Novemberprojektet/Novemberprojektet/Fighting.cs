dd449bf02070abed05ae81368ac3acc22e76fe7d
KnasBas <anton.jansson@elev.ga.ntig.se>
2019-10-21 10:51:43 +0200

    commit 2

--- Novemberprojektet/Novemberprojektet/Fighting.cs
+++ Novemberprojektet/Novemberprojektet/Fighting.cs
@@ -10,13 +10,56 @@ namespace Novemberprojektet
     {
         protected int stance = 0;
 
+        protected float hp = 0;
+
         public int GetStance(int amount)
         {
-
-
-
+            if(amount == 1)
+            {
+                stance = 1;
+            }
+            else
+            {
+                stance = 2;
+            }
 
             return stance;
+        }
+
+        public int Attack()
+        {
+            Random generator = new Random();
+
+            int damage = 0;
+
+            if (stance == 1) //Offensiv
+            {
+                damage = generator.Next(5, 15); //5 - 15
+            }
+            else if (stance == 2) //Defensiv
+            {
+                damage = generator.Next(1, 9); //1 - 9
+            }
+          
+            return damage;
+        }
+
+        public void Hurt(float amount)
+        {
+            Random generator = new Random();
+
+            int defensiveStance = generator.Next(3); // 1/3 chans
+
+            if (stance == 2 && defensiveStance == 2)
+            {
+                amount = 0;
+            }
+            else if (stance == 2 && defensiveStance != 2)
+            {
+                amount = amount * 0.5f;
+            }
+
+            hp = hp - amount;
         }
 
     }
